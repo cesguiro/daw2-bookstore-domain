a15de71e6d262bdb0b4c85446c2cad1bfe6ee2fd
César Guijarro <cesguiro@gmail.com>
2025-09-26 10:03:49 +0200

    añadido validador básico

---
+++ src/main/java/es/cesguiro/domain/validation/BookValidator.java
@@ -0,0 +1,22 @@
+package es.cesguiro.domain.validation;
+
+import es.cesguiro.domain.service.dto.BookDto;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+public class BookValidator extends Validator<BookDto>{
+
+
+    @Override
+    public List<String> validate(BookDto bookDto) {
+
+        try {
+            Field isbnField = BookDto.class.getDeclaredField("isbn");
+            this.notNull(bookDto, isbnField);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return getErrors();
+    }
+}
---
+++ src/main/java/es/cesguiro/domain/validation/Validator.java
@@ -0,0 +1,36 @@
+package es.cesguiro.domain.validation;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Validator<T> {
+
+    private List<String> errors = new ArrayList<>();
+
+    public abstract List<String> validate(T t);
+
+    protected void addError(String error) {
+        errors.add(error);
+    }
+
+    public boolean isValid() {
+        return errors.isEmpty();
+    }
+
+    public List<String> getErrors() {
+        return errors;
+    }
+
+
+    protected void notNull(T instance, Field field) throws IllegalAccessException {
+        field.setAccessible(true);
+        Object value  = field.get(instance);
+        if (value == null) {
+            addError(field.getName() + " must not be null");
+        }
+    }
+
+
+    // Other validation methods like notEmpty, maxLength, minLength, pattern, between can be added here
+}
