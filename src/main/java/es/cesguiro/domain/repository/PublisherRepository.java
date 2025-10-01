06040c308f070ff4be4a7ba86c00884853250586
CESAR GUIJARRO ROSALENY <cguijarro@0A6PC04L.ad.fpmislata.com>
2025-10-01 10:47:01 +0200

    añadido findBySlug de Publisher

--- src/main/java/es/cesguiro/domain/repository/PublisherRepository.java
+++ src/main/java/es/cesguiro/domain/repository/PublisherRepository.java
@@ -2,6 +2,7 @@ package es.cesguiro.domain.repository;
 
 import es.cesguiro.domain.repository.entity.PublisherEntity;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PublisherRepository {
